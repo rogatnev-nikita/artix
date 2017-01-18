a08f72bcd892c05dc3e8c4f4241e6a8678fd708f
Rogatnev Nikita <mail@rogatnev.ru>
2017-01-18 16:14:41 +0300

    form start

--- php/script.php
+++ php/script.php
@@ -1,10 +1,17 @@
 <?
-    $to = "go@artix.studio";
-    $subject = "Artix | Студия художественного оформления";
+    $to = "mail@rogatnev.ru"; //go@artix.studio
+    $subject = "Artix - Студия художественного оформления";
     $message = "<html><head><title>'.$subject.'</title></head>";
     $message .= "<body><p>Email: ".$_POST['email']."</p></body></html>";
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-    $headers .= "From: Отправитель <".$to.">\r\n";
+    $headers .= "From: Artix <".$to.">\r\n";
 
-    mail($to, $subject, $message, $headers);
+    $success = mail($to, $subject, $message, $headers);
+
+  if ($success){
+     echo "success";
+  }
+  else{
+      echo "invalid";
+  }
 ?>
