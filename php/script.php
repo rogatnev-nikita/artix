3387f932c67bd3e6dd32cd0d695e6fe32771c750
Rogatnev Nikita <mail@rogatnev.ru>
2017-01-24 15:28:58 +0300

    fix validation

--- php/script.php
+++ php/script.php
@@ -1,17 +1,10 @@
 <?
-    $to = "mail@rogatnev.ru"; //go@artix.studio
+    $to = "go@artix.studio";
     $subject = "Artix - Студия художественного оформления";
     $message = "<html><head><title>'.$subject.'</title></head>";
     $message .= "<body><p>Email: ".$_POST['email']."</p></body></html>";
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
     $headers .= "From: Artix <".$to.">\r\n";
 
-    $success = mail($to, $subject, $message, $headers);
-
-  if ($success){
-     echo "success";
-  }
-  else{
-      echo "invalid";
-  }
+    mail($to, $subject, $message, $headers);
 ?>
