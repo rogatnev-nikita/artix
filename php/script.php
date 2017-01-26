377221c8827a6174175947827fbca125f055e1f2
Rogatnev Nikita <mail@rogatnev.ru>
2017-01-26 19:11:58 +0300

    email fix

--- php/script.php
+++ php/script.php
@@ -1,8 +1,8 @@
 <?
-    $to = "mail@rogatnev.ru";//go@artix.studio
+    $to = "go@artix.studio";
     $subject = "Artix - Студия художественного оформления";
     $message = "<html><head><title>'.$subject.'</title></head>";
-    $message .= "<body><p>Email: ".$_POST['email']."</p><br><p>Телефон: ".$_POST['tel']."</p></body></html>";
+    $message .= "<body><p>Email: ".$_POST['email']."</p><p>Телефон: ".$_POST['tel']."</p></body></html>";
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
     $headers .= "From: Artix <".$to.">\r\n";
 
